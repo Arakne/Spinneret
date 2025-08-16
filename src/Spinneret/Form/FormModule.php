4ece53c8310c0ee30910d1cf0e185a74b98a3729
Vincent Quatrevieux <vincent.soyuz@hotmail.fr>
2025-08-16 17:57:37 +0200

    chore(web): Use autoimport for modules

--- src/Spinneret/Form/FormModule.php
+++ src/Spinneret/Form/FormModule.php
@@ -4,6 +4,7 @@ namespace Arakne\Spinneret\Form;
 
 use Arakne\Spinneret\Application\Application;
 use Arakne\Spinneret\Application\ModuleInterface;
+use Arakne\Spinneret\Container\Builder\ServiceBuilder;
 use Arakne\Spinneret\Container\Value\Reference;
 use Arakne\Spinneret\Container\Builder\ContainerBuilder;
 use Arakne\Spinneret\Form\Csrf\CsrfHelper;
@@ -13,6 +14,7 @@ use Psr\Container\ContainerInterface;
 use Quatrevieux\Form\ContainerRegistry;
 use Quatrevieux\Form\FormFactoryInterface;
 use Quatrevieux\Form\RegistryInterface;
+use Quatrevieux\Form\Validator\Constraint\ConstraintValidatorInterface;
 
 /**
  * Module for provide "vincent4vx/form" services
@@ -32,6 +34,10 @@ final class FormModule implements ModuleInterface
         $containerBuilder->register(FormFactoryLoader::class, [
             new Reference(RegistryInterface::class),
         ]);
+
+        $containerBuilder->configureInstanceOf(ConstraintValidatorInterface::class, static function (ServiceBuilder $service) {
+            $service->public();
+        });
 
         $containerBuilder->register(FormFactoryInterface::class)
             ->factory(new Reference(FormFactoryLoader::class)->method('load'))
