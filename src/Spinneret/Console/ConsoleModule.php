0f4d9a24813d159000ef74082c68d9a881628e9e
Vincent Quatrevieux <vincent.soyuz@hotmail.fr>
2025-08-20 18:36:22 +0200

    chore(console): Use ModuleInterface instead of AbstractModule

--- src/Spinneret/Console/ConsoleModule.php
+++ src/Spinneret/Console/ConsoleModule.php
@@ -2,8 +2,8 @@
 
 namespace Arakne\Spinneret\Console;
 
-use Arakne\Spinneret\Application\AbstractModule;
 use Arakne\Spinneret\Application\Application;
+use Arakne\Spinneret\Application\ModuleInterface;
 use Arakne\Spinneret\Console\Processor\RegisterConsoleCommandProcessor;
 use Arakne\Spinneret\Container\Builder\ContainerBuilder;
 use Arakne\Spinneret\Container\Builder\ServiceBuilder;
@@ -26,24 +26,23 @@ use function Arakne\Spinneret\Application\service;
  * - {@see CacheClearCommand} - Command to clear the cache
  * - {@see DebugConfigCommand} - Command to show the configuration
  */
-final class ConsoleModule extends AbstractModule
+final class ConsoleModule implements ModuleInterface
 {
     #[Override]
-    protected function configure(): void
-    {
-        $this->service(Console::class, [
-            service(Application::class),
-            [],
-        ], public: true);
-    }
-
-    #[Override]
-    protected function configureContainer(ContainerBuilder $containerBuilder): void
+    public function register(ContainerBuilder $containerBuilder): void
     {
         $containerBuilder->processor(new RegisterConsoleCommandProcessor());
         $containerBuilder->configureAttribute(AsCommand::class, static function (ServiceBuilder $service, ContainerBuilder $builder, AsCommand $attribute): void {
             $service->tag($attribute)->public();
         });
+
+        $containerBuilder
+            ->register(Console::class, [
+                service(Application::class),
+                [],
+            ])
+            ->public()
+        ;
 
         $containerBuilder->register(CacheClearCommand::class, [service(Application::class)]);
         $containerBuilder->register(DebugConfigCommand::class, [service(Application::class)]);
