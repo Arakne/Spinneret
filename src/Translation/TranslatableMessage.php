4e598a393ec614f2555574fef0e4922326682d9b
Vincent Quatrevieux <vincent.soyuz@hotmail.fr>
2026-08-02 18:16:09 +0200

    fix: TranslatableMessage $parameters typed as array<string, mixed> instead pf array<string, scalar>

--- src/Translation/TranslatableMessage.php
+++ src/Translation/TranslatableMessage.php
@@ -17,7 +17,7 @@ final readonly class TranslatableMessage implements TranslatableInterface
         /**
          * Replacement parameters
          *
-         * @var array<string, scalar>
+         * @var array<string, mixed>
          */
         public array $parameters = [],
     ) {}
